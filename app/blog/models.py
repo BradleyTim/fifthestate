0989cb4e0768f608066a6c7da412fd0aa5f4980d
BradleyTim <timbradley94@gmail.com>
2020-06-24 19:51:58 +0300

    add ckeditor to blog app

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=140)
   slug = models.CharField(max_length=140)
-  content = models.TextField()
+  content = RichTextField()
   date_posted = models.DateField(default=timezone.now)
   author = models.ForeignKey(User, on_delete=models.CASCADE)
 
