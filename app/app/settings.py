d13ed2d417b3bea5899a95037fc085215dc631ea
BradleyTim <timbradley94@gmail.com>
2020-06-25 19:54:37 +0300

    update database

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,8 +128,8 @@ STATIC_URL = '/static/'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=700)
+DATABASES['default'] = db_from_env
 
 
 # Simplified static file serving.
